b21b006b9b573db14e25f946e48a96f10e8145da
aimericsr <52284219+aimericsr@users.noreply.github.com>
2022-05-11 10:55:57 +0200

    simple token auth

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+
+	"github.com/aimericsr/social-network-auth-api/util"
+)
+
+func main() {
+	_, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config variables:", err)
+	}
+}
